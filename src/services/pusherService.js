bf0a47bcf85f8ebc29dca8b5616d7d029ed09301
Swapnil Bhavsar <hey@swapnil.dev>
2023-05-08 17:01:18 +0530

    Update pusher service to use native config

--- src/services/pusherService.js
+++ src/services/pusherService.js
@@ -3,14 +3,26 @@ import useAuthStore from "../store/authStore";
 
 const authStore = useAuthStore();
 
+// Custom websocket server
+// const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+//   wsHost: import.meta.env.VITE_PUSHER_HOST,
+//   wsPort: import.meta.env.VITE_PUSHER_PORT,
+//   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//   forceTLS: true,
+//   encrypted: true,
+//   disableStats: true,
+//   enabledTransports: ["ws", "wss"],
+//   channelAuthorization: {
+//     endpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
+//     headers: {
+//       Authorization: `Bearer ${authStore.token}`,
+//     },
+//   },
+// });
+
+// Pusher config
 const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
-  wsHost: import.meta.env.VITE_PUSHER_HOST,
-  wsPort: import.meta.env.VITE_PUSHER_PORT,
   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-  forceTLS: true,
-  encrypted: true,
-  disableStats: true,
-  enabledTransports: ["ws", "wss"],
   channelAuthorization: {
     endpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
     headers: {
