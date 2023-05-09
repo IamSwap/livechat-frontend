766d15e6e5a97a94f0a08872ebba2286b47189a7
Swapnil Bhavsar <hey@swapnil.dev>
2023-05-09 13:10:43 +0530

    Update pusher config

--- src/services/pusherService.js
+++ src/services/pusherService.js
@@ -4,25 +4,14 @@ import useAuthStore from "../store/authStore";
 const authStore = useAuthStore();
 
 // Custom websocket server
-// const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
-//   wsHost: import.meta.env.VITE_PUSHER_HOST,
-//   wsPort: import.meta.env.VITE_PUSHER_PORT,
-//   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//   forceTLS: true,
-//   encrypted: true,
-//   disableStats: true,
-//   enabledTransports: ["ws", "wss"],
-//   channelAuthorization: {
-//     endpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
-//     headers: {
-//       Authorization: `Bearer ${authStore.token}`,
-//     },
-//   },
-// });
-
-// Pusher config
 const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+  wsHost: import.meta.env.VITE_PUSHER_HOST,
+  wsPort: import.meta.env.VITE_PUSHER_PORT,
   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+  forceTLS: true,
+  encrypted: true,
+  disableStats: true,
+  enabledTransports: ["ws", "wss"],
   channelAuthorization: {
     endpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
     headers: {
@@ -30,5 +19,16 @@ const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     },
   },
 });
+
+// Pusher config
+// const pusherService = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+//   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//   channelAuthorization: {
+//     endpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
+//     headers: {
+//       Authorization: `Bearer ${authStore.token}`,
+//     },
+//   },
+// });
 
 export default pusherService;
